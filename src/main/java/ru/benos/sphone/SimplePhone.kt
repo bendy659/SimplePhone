d681aa3025fb4aaff42737b479fa0b8ec2ef7189
_BENDY659_ <denisglazyrin13@gmail.com>
2025-02-28 13:34:43 +0900

    reinstall

---
+++ src/main/java/ru/benos/sphone/SimplePhone.kt
@@ -0,0 +1,26 @@
+package ru.benos.sphone
+
+import com.mojang.logging.LogUtils
+import io.netty.handler.logging.LogLevel
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import org.slf4j.LoggerFactory
+
+@Mod(SimplePhone.MODID)
+class SimplePhone {
+  companion object {
+    const val MODID = "sphone"
+    val LOGGER = LoggerFactory.getLogger(MODID)
+  }
+
+  @SubscribeEvent
+  private fun initCommon(e: FMLCommonSetupEvent) {
+    LOGGER.info("Init common...")
+  }
+  @SubscribeEvent
+  private fun initClient(e: FMLClientSetupEvent) {
+    LOGGER.info("Init client...")
+  }
+}
